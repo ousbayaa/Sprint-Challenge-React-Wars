0e7e6d056488a41a680e81afc755bd2a4fffe289
Ous Bayaa <bayaaous@gmail.com>
2020-02-21 10:13:27 -0800

    completed styling using styled components

--- starwars/src/components/Characters/Character.js
+++ starwars/src/components/Characters/Character.js
@@ -1,15 +1,35 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Person = styled.div `
+    color: crimson;
+    border: 3px inset dodgerblue;
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+    margin-right: 30%;
+    margin-left: 30%;
+    background-color: rgba(255,182,193, 0.5);
+`;
+
+const Nombre = styled.h3 `
+    color: lightgreen;
+`
+
+const Sex = styled.p `
+    color: mediumpurple;
+`
 
 const Character = props => {
     
 
   return (
-    <div className="Character"> 
-        <h3>Name: {props.name}</h3>
-        <p>Gender: {props.gender}</p>
+    <Person className="Character"> 
+        <Nombre>Name: {props.name}</Nombre>
+        <Sex>Gender: {props.gender}</Sex>
         <p>Birth Year: {props.birth}</p>
-    </div>
+    </Person>
   );
 }
 
